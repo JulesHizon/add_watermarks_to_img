8805d2380b70a056f34fc4cacaa54bac7c3e4d2a
Jules Hizon <hizonjules@gmail.com>
2022-02-06 02:24:42 -0800

    vers. 1 completed

--- watermarker.py
+++ watermarker.py
@@ -1,34 +1,8 @@
 import cv2
 
-def transparent_overlay(src, overlay, pos=(0, 0), scale=1):
+def add_watermark(img_path, watermark_text='Jules Hizon', text_color=(255,255,255), pos=(100,100), font=cv2.FONT_HERSHEY_SIMPLEX,):
 
-    overlay = cv2.resize(overlay, (0, 0), fx=scale, fy=scale)
-    h, w, _ = overlay.shape  # Size of foreground
-    rows, cols, _ = src.shape  # Size of background Image
-    y, x = pos[0], pos[1]  # Position of foreground/overlay image
+    img_to_watermark = cv2.imread(img_path)
+    wm_img = cv2.putText(img=img_to_watermark, text=watermark_text, org=pos, fontFace=font, fontScale=1, color=text_color, thickness=2)
 
-    # loop over all pixels and apply the blending equation
-    for i in range(h):
-        for j in range(w):
-            if x + i >= rows or y + j >= cols:
-                continue
-            alpha = float(overlay[i][j][3] / 255.0)  # read the alpha channel
-            src[x + i][y + j] = alpha * overlay[i][j][:3] + (1 - alpha) * src[x + i][y + j]
-    return src
-
-
-def add_watermark(LogoImage,MainImage,opacity,pos=(10,100),):
-    opacity = opacity / 100
-
-    OriImg = cv2.imread(MainImage, -1)
-    waterImg = cv2.imread(LogoImage, -1)
-
-    tempImg = OriImg.copy()
-
-    overlay = transparent_overlay(tempImg, waterImg, pos)
-    output = OriImg.copy()
-
-    watermarked_img = cv2.addWeighted(overlay, opacity, output, 1 - opacity, 0, output)
-
-    return watermarked_img
-
+    return wm_img
--- watermarker_gui.py
+++ watermarker_gui.py
@@ -1,46 +1,41 @@
 import tkinter as tk
-from tkinter.filedialog import askopenfilename, askopenfilenames, asksaveasfilename
+from tkinter.filedialog import askopenfilenames
 import watermarker as wm
 import cv2
 
-WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 250
 
 root = tk.Tk()
 root.title('Watermarker App')
 root.minsize(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
 
-#Steps
-# 1) Build the window GUI
-# 2) Build out the buttons to be pressed, to upload/browse files to watermark
-# 3) Build the watermarking algorithm using OpenCV2
-# 4) Link the GUI with the algorithm
-# 5) Build path variables to append '_watermarked' to watermarked_images and put in new folder
+def update_instructions():
+    instructions.config(text=f'{len(files)} picture(s) loaded')
 
 def add_files_to_watermark():
     global files
 
-    added_files = False
-
     filetypes = [('All files', '*.*')]
     files = askopenfilenames(filetypes=filetypes)
+    if files:
+        instructions.config(text=f'{len(files)} picture(s) loaded')
 
 def watermark_files():
     
     if files:
         for file in files:
-            wm_img = wm.add_watermark('watermark-01.png', file, opacity=1, pos=(0,0))
+            wm_img = wm.add_watermark(file)
             file_path = file.split("/")[-1]
             cv2.imwrite(f"watermarked_{file_path}", wm_img)
+        instructions.config(text=f"{len(files)} picture(s) successfully watermarked!")
 
-#STEP 1
 instructions = tk.Label(text = "Choose your image(s) to watermark!", font = ('Calibri', 20,))
 add_files = tk.Button(root, text="Add Pictures", command=add_files_to_watermark)
 watermark_btn = tk.Button(root, text="Watermark!", command=watermark_files)
 
-#PLACE
-instructions.place(x=0, y=20)
-add_files.place(x=0, y=70)
-watermark_btn.place(x=0, y=100)
+instructions.place(x=25, y=20)
+add_files.place(x=200, y=70)
+watermark_btn.place(x=200, y=100)
 
 root.mainloop()
